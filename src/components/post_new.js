befceeb1bede069a6bfe5b666bf453ed19704f00
MSturges <Sturges3232@gmail.com>
2016-11-03 07:30:58 -0600

    added redux form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostNew extends Component {
   render() {
+    // const { handleSubmit } = this.props.handleSubmit
+    // const title = this.props.fields.title
+    // this is just es6 way of handling everything
+    const { fields: { title, categories, content } , handleSubmit } = this.props;
+
     return (
-      <div> Create Form </div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title}/>
+        </div>
+
+        <div className="form-group">
+          <label>categories</label>
+          <input type="text" className="form-control" {...categories}/>
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" {...content}/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostNew;
+// connect: 1st argument is mapStatetoProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+// injecting helpers inside of this.props
+export default reduxForm({
+  form: 'PostNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostNew);
